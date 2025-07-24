4120a4ec5d2bb51f9ccb28f95cfd50ebcf074349
mclinmolina <mclinmolina@gmail.com>
2025-07-24 23:35:20 +0800

    commit

--- api/app/Http/Controllers/ArticlesController.php
+++ api/app/Http/Controllers/ArticlesController.php
@@ -4,7 +4,18 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+use app\Models\Article;
+use app\Http\Resources\ArticleResource;
 class ArticlesController extends Controller
 {
-    //
+    public function showArticles(Request $request)
+    {
+        $request = Article::byHashOrFail($request);
+        return new ArticleResource ($request);
+    }
+
+    public function createArticle(Request $request) 
+    {
+
+    }
 }
