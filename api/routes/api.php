d4fecf7480d50422557eaa0d461f82a6c6c8ef52
mclinmolina <mclinmolina@gmail.com>
2025-08-22 19:56:55 +0800

    feat: added Authcontroller.
    setup laravel sanctum for login sessions,
    fixed nuxt ui version compatibility,
    updated api routes by adding auth routes and applying auth sanctum to routes.

--- api/routes/api.php
+++ api/routes/api.php
@@ -5,17 +5,25 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ArticlesController;
 use App\Http\Controllers\CategoriesController;
 use App\Http\Controllers\TagsController;
+use App\Http\Controllers\AuthenticationController;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 
+Route::post('/login', [AuthenticationController::class, 'login']);
+Route::post('/register', [AuthenticationController::class, 'register']);
+Route::post('/logout', [AuthenticationController::class, 'login']);
+
 Route::get('articles', [ArticlesController::class, 'showArticles']);
-Route::post('articles', [ArticlesController::class, 'createArticle']);
-Route::patch('articles/{hash}', [ArticlesController::class, 'updateArticle']);
-Route::delete('articles/{hash}', [ArticlesController::class, 'deleteArticle']);
-Route::put('articles/{hash}', [ArticlesController::class, 'restoreArticle']);
 
-Route::post('category', [CategoriesController::class, 'createCategory']);
+Route::middleware('auth:sanctum')->group(function() {
+    Route::post('articles', [ArticlesController::class, 'createArticle']);
+    Route::patch('articles/{hash}', [ArticlesController::class, 'updateArticle']);
+    Route::delete('articles/{hash}', [ArticlesController::class, 'deleteArticle']);
+    Route::put('articles/{hash}', [ArticlesController::class, 'restoreArticle']);
 
-Route::post('tags', [TagsController::class, 'createTag']);
+    Route::post('category', [CategoriesController::class, 'createCategory']);
+
+    Route::post('tags', [TagsController::class, 'createTag']);
+});
